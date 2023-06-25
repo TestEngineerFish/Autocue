597c5b9b9a1da0f8073863811e105e599de9e140
sam <916878440@qq.com>
2023-06-25 22:03:52 +0800

    Initial Commit

---
+++ Autocue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autocue
+//
+//  Created by 老沙 on 2023/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
